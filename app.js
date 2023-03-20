4a49b3cc4fc7508feb52d535e7baf95aaa8d50be
Naveen Majji <majjinaveen1436@gmail.com>
2023-03-20 07:05:31 +0000

    Today's Date

---
+++ app.js
@@ -0,0 +1,9 @@
+let express = require("express")
+let app = express()
+
+app.get("/",(request,response)=>{
+    let date = new Date()
+    response.send(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`)
+})
+
+module.exports = app;
